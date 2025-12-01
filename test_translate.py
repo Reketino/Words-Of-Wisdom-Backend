f10e14f4ee0ed20dea8c00866b4959cd91ca1f2a
Reketino <bjornevensk8@gmail.com>
2025-12-01 21:23:21 +0100

    forbedret versjon

--- test_translate.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-r = requests.post(
-    "https://translate.astian.org/translate",
-    json={"q": "Hello world", "source": "en", "target": "no"}
-)
-
-print(r.status_code, r.text)
